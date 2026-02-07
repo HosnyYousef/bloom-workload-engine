c14d1ad66f3669901af7f9bf2931557fda4d2884
Yousef Hosny <94009699+HosnyYousef@users.noreply.github.com>
2026-02-07 11:19:26 -0800

    components and their relevant files

---
+++ bloomspaceapp/frontend/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+    return (
+        <div>
+            <header className="header">
+                <div className="logo left">
+                    <i id="menu" className="material=icons">menu</i>
+                    <img src="https://www.freecodecamp.org/news/content/images/2022/01/yt-logo.png" alt="YouTube logo" 
+                    />
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default Header
--- bloomspaceapp/frontend/src/App.jsx
+++ bloomspaceapp/frontend/src/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      App
+      <Navbar />
+      <Header />
+    </div>
+  );
+};
 
-export default App
+export default App;
