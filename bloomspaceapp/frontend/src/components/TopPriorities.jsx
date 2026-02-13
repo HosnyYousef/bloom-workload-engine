6f90e39af80cfaf3ec32e6f6ea9e383ec5eaa220
Yousef Hosny <94009699+HosnyYousef@users.noreply.github.com>
2026-02-12 18:32:21 -0800

    forTomorrow.jsx and topPriorities.jsx code added

--- bloomspaceapp/frontend/src/components/TopPriorities.jsx
+++ bloomspaceapp/frontend/src/components/TopPriorities.jsx
@@ -3,17 +3,17 @@ import { useState } from 'react'
 const TopPriorities = () => {
     // State: stores our list of tasks
     const [tasks, setTasks] = useState([
-        { id: 1, text: 'help friend apply for vistor VISA', completed: false },
+        { id: 1, text: 'Help friend apply for vistor VISA', completed: false },
         { id: 2, text: 'Work on project', completed: false },
         { id: 3, text: 'Process Business Data', completed: false }
-    ])
+    ]);
 
     // State: stores the new task being typed
     const [newTask, setNewTask] = useState('');
 
     // Funtion: Toggle task completion (check/uncheck)
     const toggleTask = (id) => {
-        setTasts(tasks.map(task =>
+        setTasks(tasks.map(task =>
             task.id === id ? { ...task, completed: !task.completed } : task
         ))
     }
