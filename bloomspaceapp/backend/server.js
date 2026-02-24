f4ad89e41b34fd89cb26d3a383281a7740e8c2b9
Yousef Hosny <94009699+HosnyYousef@users.noreply.github.com>
2026-02-23 20:31:54 -0800

    updated frontend and backend packages to run server

--- bloomspaceapp/backend/server.js
+++ bloomspaceapp/backend/server.js
@@ -17,6 +17,9 @@ app.use(cors()); // Enable CORS for frontend
 app.use(express.json()) // Prase JSON bodies
 app.use(express.urlencoded({ extended: false }));
 
+// app.use('/api/auth', require('./routes/auth'));
+// app.use('/api/tasks', require('./routes/tasks'));
+
 // Test route
 app.get('/', (req, res) => {
     res.json({ message: 'BloomSpace API is running!'})
